ab31a46b5852bffbc3712a34b52aa2f00608c4dc
mrjulio <mrjulio@gmail.com>
2014-02-12 19:00:46 +0200

    update ssh

--- ssh/install.sh
+++ ssh/install.sh
@@ -12,7 +12,7 @@ then
     chown $BSD_USER /home/$BSD_USER/.ssh
     cp $SCRIPT_PATH/ssh/authorized_keys /home/$BSD_USER/.ssh/
     sed -i "" 's/#Port 22/Port '$SSH_PORT'/g' /etc/ssh/sshd_config
-    sed -i "" 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+    sed -i "" 's/PermitRootLogin no/PermitRootLogin no/g' /etc/ssh/sshd_config
     sed -i "" 's/#PasswordAuthentication no/PasswordAuthentication no/g' /etc/ssh/sshd_config
     sed -i "" 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
     sed -i "" 's/#UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
